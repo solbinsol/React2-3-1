7f530f9b894d50768a88938f182fb7f4d9d3705a
solbinsol <qkaejwnj@naver.com>
2023-11-16 16:28:34 +0900

    ㄴㅁㅇㅁ

--- signbook/src/pages/_app.js
+++ signbook/src/pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import Head from 'next/head';
+import { ApolloProvider } from '@apollo/client';
+import { useApollo } from '../lib/apollo';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const apolloClient = useApollo(pageProps.initialApolloState || {});
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Head>
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
